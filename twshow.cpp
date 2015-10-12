be170179bfb57071582b9849220ceba4ebde1cc6
artfulnatalie <artful.natalie@bk.ru>
2015-10-12 19:11:28 +0300

    Create twshow.cpp

---
+++ twshow.cpp
@@ -0,0 +1,15 @@
+#include "StdAfx.h" 
+#include "tvshow.h" 
+#include <iostream>
+
+using namespace std;  
+TVshow::TVshow(void)
+{
+	cout << "вызывается конструктор тел. программы" << endl;
+}
+
+TVshow::~TVshow(void)
+{
+	cout << "вызывается деструктор тел.программы" << endl;
+}
+
